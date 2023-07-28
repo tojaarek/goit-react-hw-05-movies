a1aa7b6d2b799b7bca07964a38229b6757aeb95b
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-07-28 10:24:34 +0200

    eslint comment

--- src/components/SearhResults/SearchResults.jsx
+++ src/components/SearhResults/SearchResults.jsx
@@ -20,6 +20,7 @@ const SearchResults = ({ movieName }) => {
 
   useEffect(() => {
     getMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movieName]);
 
   return <div>{/* Wyświetl wyniki wyszukiwania */}</div>;
